a785c17a28264f2db4fba4cac3fa1e8c84cf2b72
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2024-02-16 18:33:01 -0500

    add copy to the site, implement markdown to jsx

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -1,8 +1,11 @@
 import { Fragment } from 'react';
 
 import { motion } from 'framer-motion';
+import Markdown from 'markdown-to-jsx';
 
 import ShimmerBorderCard from '@/components/shimmer-border-card';
+import { useTheme } from '@/contexts/theme-context';
+import cn from '@/util/cn';
 
 const experience = [
   {
@@ -10,25 +13,33 @@ const experience = [
     title: 'TimePlay',
     position: 'Lead Fullstack Developer',
     date: '2022 - Present',
-    description: `- Lead a team of 5 developers to build a new platform for TimePlay using React, Next.js, and TailwindCSS <br/> 
-      - Built a new platform for TimePlay using React, Next.js, and TailwindCSS <br/>
-      - Lead a team of 5 developers to build a new platform for TimePlay using React, Next.js, and TailwindCSS`,
+    description: [
+      'Spearheaded the development of fully **accessible mobile web applications**, adhering to **WCAG 2.0 standards** and delivering seamless user experiences with **React**, **Typescript**, **WebSocket**, and **Tailwind CSS**. Leveraged **Framer Motion** to ensure smooth interactions and transitions, showcasing a meticulous **attention to detail** and commitment to **user satisfaction**.',
+      '**Directed a proficient team of developers** in the successful delivery of multiple web applications. Demonstrated exceptional **leadership and communication skills**, fostering a collaborative environment conducive to achieving project milestones and exceeding client expectations.',
+      'Exhibited expertise in **full-stack development**, seamlessly integrating front-end and back-end technologies such as **Typescript**, **React.js**, **Next.js**, **Node.js**, **MongoDB**, **Docker**, and more. Applied adept problem-solving abilities to overcome technical and design challenges, ensuring the **robustness** and **scalability** of software solutions.',
+    ],
   },
   {
     logo: 'https://via.placeholder.com/150',
     title: 'Functionland',
     position: 'Web Developer | UX Designer',
-    date: '2022 - Present',
-    description:
-      'Lead a team of 5 developers to build a new platform for TimePlay using React, Next.js, and TailwindCSS',
+    date: '2021 - 2022',
+    description: [
+      'Collaborated with business and marketing teams to align website design with strategic objectives, enhancing value creation. Performed **agile-based** front-end maintenance to improve website **functionality** and **user engagement**.',
+      '**Led the design and development** of an innovative, user-friendly landing page, incorporating dynamic card components and a responsive carousel using **Svelte, JavaScript, HTML5, and CSS3**',
+      '**Directed a team of three designers** in producing high-quality media content, ensuring consistency and impact across digital platforms. **Reviewed design mockups** for technical feasibility and optimization, ensuring effective presentation on both mobile and desktop environments',
+    ],
   },
   {
     logo: 'https://via.placeholder.com/150',
     title: 'Diamond Schmitt',
     position: 'Architect | Computational Designer',
-    date: '2022 - Present',
-    description:
-      'Lead a team of 5 developers to build a new platform for TimePlay using React, Next.js, and TailwindCSS',
+    date: '2021 - 2022',
+    description: [
+      'Implemented **evolutionary algorithms** to **optimize tower placement and geometry**, enhancing urban space utilization and architectural aesthetics.',
+      'Engineered an automated system for **simulating shadow impacts**, leveraging **weather data** to provide comprehensive environmental analysis.',
+      'Developed an innovative algorithm focused on **maximizing daylight access** and **minimizing heat loss**, contributing to **energy efficiency in high-rise residential buildings**.',
+    ],
   },
 ];
 
@@ -37,49 +48,70 @@ const education = [
     logo: 'https://via.placeholder.com/150',
     location: 'University of Waterloo',
     degree: 'Master of Architecture',
-    description:
-      'Lead a team of 5 developers to build a new platform for TimePlay using React, Next.js, and TailwindCSS',
   },
 ];
 
 const aboutMe = {
   logo: 'https://via.placeholder.com/150',
-  description: `I am a full-stack developer and UX designer with a passion for creating beautiful and functional web applications. I have experience working with a wide range of technologies and frameworks, and I am always looking to learn new things and improve my skills. I am a creative problem solver with a keen eye for detail, and I am always looking for new and exciting projects to work on.`,
+  description: `**Lead Fullstack Developer** based in **Toronto, Canada**, with a unique blend of engineering and design expertise stemming from a **Master of Architecture** degree from the **University of Waterloo**. Leveraging a diverse skill set honed through architectural innovation and hobbyist coding projects, I possess strong **communication**, **critical thinking**, and **problem-solving** abilities. From utilizing machine learning to craft emotion-aware architecture to automating video games and backtesting trading strategies, I bring **creativity** and **technical proficiency** to every project I undertake.`,
 };
 
 const technologies = [
+  'Typescript',
+  'Javascript',
   'React',
   'Next.js',
+  'Svelte',
+  'Framer Motion',
+  'D3.js',
+  'Three.js',
+  'WebSocket',
+  'Docker',
+  'Rest API',
   'TailwindCSS',
   'Node.js',
   'Express',
   'MongoDB',
   'Figma',
-  'Adobe XD',
   'Photoshop',
   'Illustrator',
-  'After Effects',
-  'Premiere Pro',
-  'Blender',
-  'Unity',
-  'Unreal Engine',
+  'Rhinoceros 3D',
 ];
 
 const skills = [
-  { name: 'Leadership', level: 4 },
+  { name: 'Leadership', level: 5 },
   { name: 'Frontend Development', level: 5 },
   { name: 'Backend Development', level: 4 },
   { name: 'UX Design', level: 4 },
   { name: '3D Modeling', level: 4 },
 ];
 
+const h1Styles = {
+  light: 'text-slate-400',
+  dark: 'text-slate-300',
+  candy: 'text-teal-500',
+};
+
+const h2Styles = {
+  light: 'text-slate-600',
+  dark: 'text-slate-500',
+  candy: 'text-teal-400',
+};
+
 const ResumePage: React.FC = () => {
+  const { theme } = useTheme();
+
   return (
     <div className="flex h-screen w-screen items-center justify-center gap-6 overflow-hidden px-20 pb-20 pt-32">
       <div className="flex justify-center gap-4">
         <div className="flex w-1/2 flex-col gap-4">
           <ShimmerBorderCard scaleOnHover>
-            <h1 className="relative z-10 mb-4 w-full font-display text-5xl font-bold text-slate-400">
+            <h1
+              className={cn(
+                'mb-4 w-full font-display text-5xl font-bold ',
+                h1Styles[theme]
+              )}
+            >
               Experience
             </h1>
             {experience.map((item, index) => (
@@ -92,31 +124,28 @@ const ResumePage: React.FC = () => {
               />
             ))}
           </ShimmerBorderCard>
-          <ShimmerBorderCard scaleOnHover>
-            {education.map((item, index) => (
-              <div key={index}>
-                <h1 className="relative z-10 mb-4 w-full  font-display text-5xl font-bold text-slate-400">
-                  Education
-                </h1>
-                <CardContent
-                  key={index}
-                  title={item.location}
-                  subtitle={item.degree}
-                  description={item.description}
-                />
-              </div>
-            ))}
-          </ShimmerBorderCard>
         </div>
         <div className="flex w-1/2 flex-col gap-4">
           <ShimmerBorderCard scaleOnHover>
-            <h1 className="relative z-10 mb-4 w-full  font-display text-5xl font-bold text-slate-400">
+            <h1
+              className={cn(
+                'mb-4 w-full font-display text-5xl font-bold ',
+                h1Styles[theme]
+              )}
+            >
               About me
             </h1>
-            <CardContent description={aboutMe.description} />
+            <p className="font-sans text-sm text-slate-400">
+              <Markdown>{aboutMe.description}</Markdown>
+            </p>
           </ShimmerBorderCard>
           <ShimmerBorderCard scaleOnHover>
-            <h1 className="relative z-10 mb-4 w-full  font-display text-5xl font-bold text-slate-400">
+            <h1
+              className={cn(
+                'mb-4 w-full font-display text-5xl font-bold ',
+                h1Styles[theme]
+              )}
+            >
               Skills
             </h1>
             <div className="flex w-full flex-col gap-2">
@@ -125,7 +154,12 @@ const ResumePage: React.FC = () => {
                   key={index}
                   className="items-between flex w-full justify-between gap-4"
                 >
-                  <h2 className="font-sans text-sm font-bold uppercase text-slate-800">
+                  <h2
+                    className={cn(
+                      'font-sans text-sm font-bold uppercase',
+                      h2Styles[theme]
+                    )}
+                  >
                     {item.name}
                   </h2>
                   <div className="h-4 w-40 rounded-full bg-slate-200">
@@ -141,7 +175,12 @@ const ResumePage: React.FC = () => {
             </div>
           </ShimmerBorderCard>
           <ShimmerBorderCard scaleOnHover>
-            <h1 className="relative z-10 mb-4 w-full  font-display text-5xl font-bold text-slate-400">
+            <h1
+              className={cn(
+                'mb-4 w-full font-display text-5xl font-bold ',
+                h1Styles[theme]
+              )}
+            >
               Technologies
             </h1>
             <div className="flex flex-wrap gap-2">
@@ -153,6 +192,28 @@ const ResumePage: React.FC = () => {
                   {item}
                 </div>
               ))}
+            </div>
+          </ShimmerBorderCard>
+          <ShimmerBorderCard scaleOnHover className="">
+            <div className="flex h-full w-full items-center justify-between">
+              <h1
+                className={cn(
+                  'font-display text-5xl font-bold ',
+                  h1Styles[theme]
+                )}
+              >
+                Education
+              </h1>
+              <div>
+                <h2
+                  className={cn(
+                    'font-sans text-sm font-bold uppercase ',
+                    h2Styles[theme]
+                  )}
+                >
+                  {education[0].degree} | {education[0].location}
+                </h2>
+              </div>
             </div>
           </ShimmerBorderCard>
         </div>
@@ -167,7 +228,7 @@ type CardContentProps = {
   title?: string;
   subtitle?: string;
   date?: string;
-  description?: string;
+  description?: string[];
 };
 
 const CardContent: React.FC<CardContentProps> = ({
@@ -176,29 +237,35 @@ const CardContent: React.FC<CardContentProps> = ({
   date,
   description,
 }) => {
+  const { theme } = useTheme();
   return (
     <div className="flex flex-col">
       {title && (
-        <h1 className="font-display text-xl font-bold text-slate-800">
+        <h2 className={cn('font-sans text-lg font-bold', h2Styles[theme])}>
           {title}
-        </h1>
+        </h2>
       )}
       {subtitle && (
-        <h2 className="font-sans text-sm font-bold uppercase text-slate-800">
+        <h2
+          className={cn(
+            'font-sans text-sm font-bold uppercase',
+            h2Styles[theme]
+          )}
+        >
           {subtitle}
         </h2>
       )}
-      {date && <h2 className="font-sans text-sm text-slate-400">{date}</h2>}
+      {date && (
+        <h3 className={cn('font-sans text-sm ', h1Styles[theme])}>{date}</h3>
+      )}
       {description && (
-        <p className="mb-6 mt-2 font-sans text-sm leading-6 text-slate-400">
-          {/* break description on <br/> */}
-          {description.split('<br/>').map((item, index) => (
-            <Fragment key={index}>
-              {item}
-              <br />
-            </Fragment>
+        <ul className="mb-4 mt-2 list-inside list-disc font-sans text-sm text-slate-400">
+          {description.map((item, index) => (
+            <li key={index}>
+              <Markdown>{item}</Markdown>
+            </li>
           ))}
-        </p>
+        </ul>
       )}
     </div>
   );
