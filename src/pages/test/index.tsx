096d87e88fa4b12c6e7859d59d790bd7dfcc609c
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2025-10-22 06:23:07 -0400

    delete the project and start new with the latest nextJS and react

--- src/pages/test/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import SwirlingColorsBg from '@/components/swirling-colors-bg';
-
-const TestPage: React.FC = () => {
-  return <SwirlingColorsBg />;
-};
-
-export default TestPage;
