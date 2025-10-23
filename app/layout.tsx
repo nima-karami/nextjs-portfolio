8ca0fc9b92f8596f1132e16064e79ec1e001cdce
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2025-10-23 11:01:09 -0400

    add speed insights and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Jura, Roboto } from 'next/font/google';
 
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 import Navbar from './components/navbar';
 import './globals.css';
 
@@ -31,6 +34,8 @@ export default function RootLayout({
       <body className={`${jura.variable} ${roboto.variable} antialiased`}>
         <Navbar />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
