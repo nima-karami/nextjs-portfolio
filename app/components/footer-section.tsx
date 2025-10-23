3db8b055c37a287e060ca01aa91796603effcf8e
Nima Karami <94768955+nima-karami@users.noreply.github.com>
2025-10-23 06:37:51 -0400

    add social media icons to the footer

--- app/components/footer-section.tsx
+++ app/components/footer-section.tsx
@@ -1,4 +1,8 @@
 import Image from 'next/image';
+import Link from 'next/link';
+
+import { SlSocialLinkedin } from 'react-icons/sl';
+import { VscGithubAlt } from 'react-icons/vsc';
 
 import Container from './container';
 
@@ -12,7 +16,10 @@ function FooterSection() {
       </div>
       <section id="footer" className="border-secondary w-full border-b">
         <Container className="flex h-16 justify-between">
-          <div className="border-secondary flex h-full items-center border-r px-4">
+          <Link
+            href="/"
+            className="border-secondary hover:bg-secondary flex h-full items-center border-r px-4 transition duration-300"
+          >
             <Image
               src="/images/logo-white.png"
               alt="Logo"
@@ -20,11 +27,27 @@ function FooterSection() {
               height={32}
               className="inline-block"
             />
-          </div>
-          <div className="border-secondary flex h-full items-center border-l px-4">
-            <p className="text-sm text-gray-400">
-              © 2023 Your Company. All rights reserved.
-            </p>
+          </Link>
+          <div className="flex">
+            <div className="border-secondary flex h-full items-center border-l px-4">
+              <p className="text-sm text-gray-400">
+                © {new Date().getFullYear()} Nima Karami. All rights reserved.
+              </p>
+            </div>
+            <div className="flex h-full items-center">
+              <Link
+                href="https://www.linkedin.com/in/nima-karami/"
+                className="border-secondary hover:bg-secondary flex h-full items-center border-l px-4 text-sm text-gray-400 transition duration-300"
+              >
+                <SlSocialLinkedin size={20} />
+              </Link>
+              <Link
+                href="https://github.com/nima-karami"
+                className="border-secondary hover:bg-secondary flex h-full items-center border-l px-4 text-sm text-gray-400 transition duration-300"
+              >
+                <VscGithubAlt size={20} />
+              </Link>
+            </div>
           </div>
         </Container>
       </section>
